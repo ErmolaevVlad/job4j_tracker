1ebe7a1030be73418e5e9a27c91769831483935a
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2022-09-20 16:35:11 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker.oop;
+
+public class Computer {
+
+    private boolean multiMonitor;
+    private int ssd;
+    private String cpu;
+
+    public void printInfo() {
+        System.out.println("Много монитор: " + multiMonitor);
+        System.out.println("SSd: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+    }
+
+    public Computer() {
+    }
+
+    public Computer(boolean multiMonitor, int ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public static void main(String[] args) {
+        Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
+        computer.printInfo();
+        Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
+        comp.printInfo();
+        Computer third = new Computer();
+        third.printInfo();
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printReport() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error first = new Error(true, 1, "Error message");
+        first.printReport();
+        Error second = new Error(false, 2, "Error mail");
+        second.printReport();
+        Error third = new Error();
+        third.printReport();
+    }
+}
