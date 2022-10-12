872c2cc9c5048da660c998887924d19989286097
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2022-10-12 17:05:27 +0700

    1.9. Статические и не статические методы [#504806]

--- src/test/java/ru/job4j/oop/CalculatorTest.java
+++ src/test/java/ru/job4j/oop/CalculatorTest.java
@@ -10,43 +10,43 @@ class CalculatorTest {
     @Test
     void whenSumX5toY6Then11() {
         int y = 6;
-        int extended = 11;
+        int expected = 11;
         int rsl = Calculator.sum(y);
-        assertThat(rsl).isEqualTo(extended);
+        assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenX5Y6ThenMultiply30() {
         int y = 6;
         Calculator calculator = new Calculator();
-        int extended = 30;
+        int expected = 30;
         int rsl = calculator.multiply(y);
-        assertThat(rsl).isEqualTo(extended);
+        assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenY9X5ThenMinus4() {
         int y = 9;
-        int extended = 4;
+        int expected = 4;
         int rsl = Calculator.minus(y);
-        assertThat(rsl).isEqualTo(extended);
+        assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenY12X6ThenDivide2() {
         int y = 10;
         Calculator calculator = new Calculator();
-        int extended = 2;
+        int expected = 2;
         int rsl = calculator.divide(y);
-        assertThat(rsl).isEqualTo(extended);
+        assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenY6X5ThenSumAllOperation72() {
         int y = 10;
         Calculator calculator = new Calculator();
-        int extended = 72;
+        int expected = 72;
         int rsl = calculator.sumAllOperation(y);
-        assertThat(rsl).isEqualTo(extended);
+        assertThat(rsl).isEqualTo(expected);
     }
 }
