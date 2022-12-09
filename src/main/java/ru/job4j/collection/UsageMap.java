5846cd9498ea5d2fb4d54d34fe7147993ee63b8e
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2022-12-09 09:41:40 +0700

    0. Map, HashMap [#212652 #368865]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("ivanov@mail.ru", "Ivan Ivanov");
+        map.put("sidorov@yandex.ru", "Petr Sidorov");
+        map.put("parsentev@yandex.ru", "Alexandr Arsentev");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
