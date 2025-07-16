94ee60882262e7b197e350f9ba6f71686bc50b8e
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2025-07-16 15:33:59 +0700

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,12 +34,14 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store tracker = new SqlTracker();
+        Store tracker = new MemTracker();
         List<UserAction> actions = Arrays.asList(
                 new CreateAction(output),
+                new CreateManyItems(output),
                 new ShowAllItems(output),
                 new EditItem(output),
                 new DeleteItem(output),
+                new DeleteAllItems(output),
                 new FindItemById(output),
                 new FindItemsByName(output),
                 new ExitProgram(output)
